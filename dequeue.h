9ab505dea90089dab65c6c99966c49898038f383
McUsr <tommy.bollman@gmail.com>
2023-05-17 14:15:31 +0200

    You must free QueueNodePtr yourself, and you can have only one Queue

--- dequeue.h
+++ dequeue.h
@@ -1,7 +1,10 @@
-
-/* There is meant to be only one dequeue in one program,
+/* Operating and maintaining a dequeue
+ * 2023 (c) McUsr -- GPL 3.0
+ *
+ * It is meant to be only *one* dequeue in one program,
  * you'll need to rework it if your needs are different.
  */
+
 struct queueNode {      /* the queueNode type is read only! */
    void* data;                  /* stuff whatever you want into data */
    struct queueNode *nextPtr;   /* but remember to free it */
@@ -14,6 +17,8 @@ extern QueueNodePtr tailPtr;
 extern QueueNodePtr *headPtr;
 
 int isEmpty(QueueNodePtr headPtr);
+
 QueueNodePtr dequeue(QueueNodePtr *headPtr) ;
-/* your responsibility to free return QueueNodePtr */
+/* your responsibility to free() the returned QueueNodePtr!!! */
+
 void enqueue( QueueNodePtr *tailPtr, void* data) ;
